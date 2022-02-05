6961fee55f5a6082b59473d1d099320d344b76dd
qartiq <choprakartik009@gmail.com>
2022-02-05 21:39:06 +0530

    added_page-logo

---
+++ DiceGame/help.js
@@ -0,0 +1,49 @@
+// let select = ['red','green'];
+// select[3] = ['black'];
+// console.log(select[1]);
+
+// function square(number){
+//     return number*number;
+// }
+// console.log(square(2,2,2))
+
+// let name = {fname:"abhi"};
+// let aname = name;
+// name.fname = "raj";
+// console.log(aname.fname);
+
+// const obj1 = {
+//     a:10,
+//     b:15,
+//     c:18
+// };
+// const obj2 = Object.assign({c:7,d:1},obj1);
+// console.log(obj2.c,obj2.d);
+
+// var a=1;
+// var b=0;
+// while(a<=3){
+//     a++;
+//     b+=a*2;
+//     console.log(b);
+// }
+
+// var a = true+true+true*3;
+// console.log(a);
+
+
+// let sum = 0;
+// const a = [1,2,3];
+// a.forEach(getSum);
+// console.log(sum);
+// function getSum(ele){
+//     sum+=ele;
+// }
+
+
+// (function (a){
+//     return (function(){
+//         console.log(a);
+//         a=6;
+//     })()
+// })(21);
